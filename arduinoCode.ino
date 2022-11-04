64a49e51e1656fade5fe0aaf2310760c8ea9b09e
LiamFallon213 <ldavidfallon@gmail.com>
2022-11-04 15:41:04 -0600

    Update arduinoCode.ino

--- arduinoCode.ino
+++ arduinoCode.ino
@@ -1,8 +1,9 @@
 #include <TimeLib.h>
+#include <Servo.h>
 
 void setup() {
-  
-
+//  Serial.begin(9600);
+//  IrReceiver.begin(inputPin);
 };
 
 void loop() {
@@ -10,57 +11,87 @@ void loop() {
 
 };
 
-class Command{
+class Command {
   private:
     int portNumber;
     int value;
-    String commandType;
+    int type;
     time_t timeStamp;
-    
-  public:
-    void execute(){
-    //TODO: wright code to handle command execution.
-    }
-  
-    Command(int portNumberIn, int valueIn, String commandTypeIn){
-        portNumberIn = portNumber;
-        valueIn = value;
-        commandTypeIn = commandType;
-        timeStamp = now();
-    }
-    
-    int getPN(){return portNumber;};
-    
-    void setPN(int input){portNumber = input;};
-    
-    int getValue(){return value;};
-    
-    void setValue(int input){value = input;};
 
-    time_t getTimeStamp(){return timeStamp;};
-    
-    String getCommand(){return commandType;};
-    
-    void setCommand(String input){commandType = input;};
-    
+  public:
+
+    Command(int portNumberIn, int valueIn, String commandTypeIn) {
+      portNumberIn = portNumber;
+      valueIn = value;
+      commandTypeIn = type;
+      timeStamp = now();
+    }
+
+    int getPN() {
+      return portNumber;
+    };
+
+    void setPN(int input) {
+      portNumber = input;
+    };
+
+    int getValue() {
+      return value;
+    };
+
+    void setValue(int input) {
+      value = input;
+    };
+
+    time_t getTimeStamp() {
+      return timeStamp;
+    };
+
+    int getType() {
+      return type;
+    };
+
+    void setType(int input) {
+      type = input;
+    };
 };
 
-class Component{
+class Component {
   private:
     int value;
     int pin;
     bool active;
-    
+
   public:
-   int getValue(){return value;};
+    int getValue() {
+      return value;
+    };
+    int setValue(int val) {
+      value = val;
+    };
+    int getPin() {
+      return pin;
+    };
 
-   int getPin(){return pin;};
+    void setPin(int val) {
+      pin = val;
+    };
 
-   void setPin(int val){pin = val;};
-   
-   Command getCommand(){return command;};
-   
-   void setCommand(Command val){command = val;};
-   
-   int setValue(int val){value = val;};
+    void execute(Command* command) {
+      switch (command->getType()) {
+
+        case 1: writeEngine(command->getValue());//engine speed command
+          break;
+
+        case 2: writeServo(command->getType()); // Servo angle control
+          break;
+
+      }
+    };
+};
+
+class Engine: public Component {
+  public:
+    int angle;
+    int enginePosition;
 };
